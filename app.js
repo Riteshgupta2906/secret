a14b611cf710b79eccfd64b83ddb294ad71ac787
ritesh <rkgritesh50@gmail.com>
2021-08-02 15:37:14 +0530

    1

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ passport.use(User.createStrategy());
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://mysterious-eyrie-41622.herokuapp.com/auth/google/secrets",
+    callbackURL: "https://stark-bastion-06309.herokuapp.com/auth/google/secrets",
     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -77,7 +77,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "https://mysterious-eyrie-41622.herokuapp.com/auth/facebook/secrets"
+  callbackURL: "https://stark-bastion-06309.herokuapp.com/auth/facebook/secrets"
 },
 function(accessToken, refreshToken, profile, cb) {
  
